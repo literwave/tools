28ab1b004c69c18093955207f762ef76ce991d4e
unknown <xiaobo.zeng@rastar.gd>
2025-12-19 11:55:18 +0800

    add HeroIcon

---
+++ server/read_config/HeadIcon.lua
@@ -0,0 +1,11 @@
+-- author:   liter_wave 
+-- Automatic generation from -->>
+-- excel file  name: ./excel/HeadIcon.xlsx
+-- excel sheet name: HeadIcon
+return {
+    [1001] = {
+        ["HeroId"] = 1001,
+        ["Resources"] = "assets/res/ui/hero/normal/1001.png",
+        ["Sort"] = 1
+    }
+}
