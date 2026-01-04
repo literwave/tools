f01940d862c878f98d81b2588ddbf6f9fe0fe047
literwave <572354941@qq.com>
2026-01-04 14:42:25 +0800

    pto

--- server/read_config/Global.lua
+++ server/read_config/Global.lua
@@ -4,7 +4,7 @@
 -- excel sheet name: Global
 return {
     [1] = {
-        ["value2"] = {{reward_type = 3,item_type = 2,item_count = 1},{reward_type = 4,item_type = 1,item_count = 1000}}
+        ["value2"] = {{reward_type = 3,item_type = 1001,item_count = 1},{reward_type = 4,item_type = 1,item_count = 1000}}
     },
     [2] = {
         ["value1"] = 1001
