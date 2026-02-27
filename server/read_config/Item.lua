8ecd2cf4e907b8764dce7777ee014fdb4e2900b7
literwave <572354941@qq.com>
2026-02-27 16:08:25 +0800

    add conf

---
+++ server/read_config/Item.lua
@@ -0,0 +1,20 @@
+-- author:   liter_wave 
+-- Automatic generation from -->>
+-- excel file  name: ./excel/Item.xlsx
+-- excel sheet name: Item
+return {
+    [1] = {
+        ["Icon"] = 1,
+        ["BuyDiamond"] = 10,
+        ["useParams"] = {},
+        ["Kind"] = 1,
+        ["Overlap"] = 1
+    },
+    [2] = {
+        ["Icon"] = 1,
+        ["BuyDiamond"] = 10,
+        ["useParams"] = {},
+        ["Kind"] = 1,
+        ["Overlap"] = 1
+    }
+}
---
+++ server/read_config/Lottery.lua
@@ -0,0 +1,13 @@
+-- author:   liter_wave 
+-- Automatic generation from -->>
+-- excel file  name: ./excel/Lottery.xlsx
+-- excel sheet name: Lottery
+return {
+    [1] = {
+        ["CostList"] = {{cost_type=1,count=10}},
+        ["CommonPools"] = {{reward_type=1,}}
+    },
+    [10] = {
+        ["CostList"] = {{cost_type=1,count=1000}}
+    }
+}
